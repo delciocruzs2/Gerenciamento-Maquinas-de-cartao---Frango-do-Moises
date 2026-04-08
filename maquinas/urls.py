5e512e57e58e2ea5d7b2d210650f44ad4d47a5f4
Delcio Cruz <delciocruzoficial@gmail.com>
2026-04-08 18:28:11 -0300

    feact(atualizar_maquina): Atualizar implementado, com html e css, e demonstração de maquinas existente em gerenciador de maquinas

--- maquinas/urls.py
+++ maquinas/urls.py
@@ -4,5 +4,6 @@ from maquinas.views import *
 urlpatterns = [
     path('gerenciador_maquinas/', Maquina_View.as_view(), name='gerenciador_maquinas'),
     path('adicionar_maquinas/', Adicionar_Maquina_View.as_view(), name='adicionar_maquinas'),
+    path('atualizar_maquina/<int:id>/', Atualizar_Maquina_View.as_view(), name='atualizar_maquina'),
 ]
 
--- maquinas/views.py
+++ maquinas/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from maquinas.forms import *
+from maquinas.models import Maquinas_Model
 
 class Maquina_View(View):
-    def get(self, request) -> None:
-        return render(request, template_name='gerenciador.html')
+    def get(self, request):
+        maquinas_cadastradas = Maquinas_Model.objects.all()
+        return render(request, template_name='gerenciador.html', context={'maquinas': maquinas_cadastradas})
     
 
 class Adicionar_Maquina_View(View):
@@ -18,7 +21,27 @@ class Adicionar_Maquina_View(View):
 
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('gerenciador_maquinas')
         else:
             print(form.errors)
         return render(request, 'adicionar.html', {'form_adicionar':form})
+    
+    
+class Atualizar_Maquina_View(View):
+    def get(self, request, id) -> None:
+        maquina = get_object_or_404(Maquinas_Model, id_maquina=id)
+        form = Maquina_Form(instance=maquina)
+        
+        return render(request, template_name='atualizar.html',
+                      context={'form_atualizar': form, 'maquina': maquina})
+    
+    def post(self, request, id) -> redirect:
+        maquina = get_object_or_404(Maquinas_Model, id_maquina=id)
+        form = Maquina_Form(request.POST, request.FILES, instance=maquina)
+
+        if form.is_valid():
+            form.save()
+            return redirect('gerenciador_maquinas')
+        else:
+            print(form.errors)
+        return render(request, 'atualizar.html', {'form_atualizar': form, 'maquina': maquina})
